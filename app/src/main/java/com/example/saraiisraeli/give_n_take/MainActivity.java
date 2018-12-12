9973d749f7273dc3c2f6da83660d6d5dc4f557e7
SaraiIsraeli <sarai240694@gmail.com>
2018-12-12 23:32:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/saraiisraeli/give_n_take/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.saraiisraeli.give_n_take;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
