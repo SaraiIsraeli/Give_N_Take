145fc0716dc5ab5b3911ccbe41c4938f7e169128
joeyHa <joey.havia@gmail.com>
2018-12-28 15:27:23 +0200

    Added Login Page + FireBase authentication

---
+++ app/src/main/java/com/example/saraiisraeli/give_n_take/activity/BaseActivity.java
@@ -0,0 +1,50 @@
+package com.example.saraiisraeli.give_n_take.activity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import com.example.saraiisraeli.give_n_take.R;
+
+
+public class BaseActivity extends AppCompatActivity {
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    public void hideKeyboard(View view) {
+        final InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
+
+}
