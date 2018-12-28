95a4b87d049282afc6ae7b9e60ceb6a638155023
SaraiIsraeli <sarai240694@gmail.com>
2018-12-28 16:29:48 +0200

    Updated fonts&photos and started intro screen

---
+++ app/src/main/java/com/example/saraiisraeli/give_n_take/activity/login.java
@@ -0,0 +1,16 @@
+
+package com.example.saraiisraeli.give_n_take.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.saraiisraeli.give_n_take.R;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
