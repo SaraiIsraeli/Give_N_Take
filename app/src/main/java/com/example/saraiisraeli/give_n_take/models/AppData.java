216fb0305dabb72b565db5513221a71c4ff993d6
joeyHa <joey.havia@gmail.com>
2019-04-30 21:53:11 +0300

    Fix Save userToken to UserSettings
    Fix Save userToken to UserSettings

--- app/src/main/java/com/example/saraiisraeli/give_n_take/models/AppData.java
+++ app/src/main/java/com/example/saraiisraeli/give_n_take/models/AppData.java
@@ -40,9 +40,9 @@ public class AppData
         {
             try
             {
-                mDatabase.child("userSettings").child("userToken").setValue(userToken);
-                mDatabase.child("userSettings").child("userToken").child("distance").setValue(distance);
-                mDatabase.child("userSettings").child("userToken").child("Role").setValue(role);
+                mDatabase.child("userSettings").setValue(userToken);
+                mDatabase.child("userSettings").child(userToken).child("distance").setValue(distance);
+                mDatabase.child("userSettings").child(userToken).child("Role").setValue(role);
             }
             catch (Exception ex)
             {
