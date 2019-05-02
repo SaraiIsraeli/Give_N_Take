4091d180dfd546bb281f115f9a63474e0b65b980
shaiis1 <shaiusraeli7@gmail.com>
2019-05-02 20:20:20 +0300

    Adding Back button to items activity

--- app/src/main/java/com/example/saraiisraeli/give_n_take/activity/HistoryItems.java
+++ app/src/main/java/com/example/saraiisraeli/give_n_take/activity/HistoryItems.java
@@ -2,6 +2,7 @@ package com.example.saraiisraeli.give_n_take.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.saraiisraeli.give_n_take.R;
 
 public class HistoryItems extends AppCompatActivity {
 
