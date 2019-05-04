d1b4df83c520be232dae0265faba70e966592c9a
shaiis1 <shaiusraeli7@gmail.com>
2019-05-04 22:37:34 +0300

    Test

--- app/src/main/java/com/example/saraiisraeli/give_n_take/activity/HistoryItems.java
+++ app/src/main/java/com/example/saraiisraeli/give_n_take/activity/HistoryItems.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import com.example.saraiisraeli.give_n_take.R;
 
 public class HistoryItems extends AppCompatActivity {
-
+//test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
