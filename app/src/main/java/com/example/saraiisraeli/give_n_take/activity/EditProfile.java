1a998defff8c7a9ef55766eae4c1262e86ef29c1
SaraiIsraeli <sarai240694@gmail.com>
2019-06-09 20:14:55 +0300

    Avital's changes on editProfile

--- app/src/main/java/com/example/saraiisraeli/give_n_take/activity/EditProfile.java
+++ app/src/main/java/com/example/saraiisraeli/give_n_take/activity/EditProfile.java
@@ -16,8 +16,11 @@ import com.example.saraiisraeli.give_n_take.models.AppData;
 import com.example.saraiisraeli.give_n_take.models.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class EditProfile extends AppCompatActivity {
     private static final String TAG = "edit profile";
@@ -31,11 +34,16 @@ public class EditProfile extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener firebaseAuthListner;
     private DatabaseReference dbRef;
     String m_nameStr, m_phoneNumberStr;
+    private DatabaseReference mDatabaseUser_name,mDatabaseUser_phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+
+
+
         Log.d(TAG, "entered edit profile" );
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseDatabase = FirebaseDatabase.getInstance();
@@ -52,6 +60,37 @@ public class EditProfile extends AppCompatActivity {
         m_phoneNumber = (EditText) findViewById(R.id.phone);
         m_start = (Button) findViewById(R.id.btnSave);
         m_back = (Button)findViewById(R.id.btnBack);
+
+        mDatabaseUser_name = FirebaseDatabase.getInstance().getReference().child("users").child(userId).child("name");
+        mDatabaseUser_phone = FirebaseDatabase.getInstance().getReference().child("users").child(userId).child("phoneNumber");
+
+        mDatabaseUser_name.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Toast.makeText(getApplicationContext(),dataSnapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
+                m_name.setText(dataSnapshot.getValue().toString());
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        mDatabaseUser_phone.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                m_phoneNumber.setText(dataSnapshot.getValue().toString());
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+        });
+
 
         m_start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -118,4 +157,6 @@ public class EditProfile extends AppCompatActivity {
         }
         return true;
     }
+
+
 }
